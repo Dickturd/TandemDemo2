08bf6aef607d0dcdc5bdaf2896f2fc0674783833
Peter <pshepherd10@gmail.com>
2017-05-15 16:12:10 -0500

    penis

---
+++ app/src/main/java/com/sourcey/TandemDemo/ConnectionsActivity.java
@@ -0,0 +1,15 @@
+package com.sourcey.TandemDemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class ConnectionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connections);
+    }
+}
--- app/src/main/java/com/sourcey/TandemDemo/HomeActivity.java
+++ app/src/main/java/com/sourcey/TandemDemo/HomeActivity.java
@@ -1,137 +1,75 @@
 package com.sourcey.TandemDemo;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.sourcey.TandemDemo.adapter.SlidingMenuAdapter;
-import com.sourcey.TandemDemo.fragment.Fragment1;
-import com.sourcey.TandemDemo.fragment.Fragment2;
-import com.sourcey.TandemDemo.fragment.Fragment3;
-import com.sourcey.TandemDemo.model.ItemSlideMenu;
-
-
-import java.util.ArrayList;
-import java.util.List;
+import butterknife.Bind;
 
 public class HomeActivity extends AppCompatActivity {
-    private List<ItemSlideMenu> listSliding;
-    private SlidingMenuAdapter adapter;
-    private ListView listViewSliding;
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
-    private DatabaseReference mDatabase;
+
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_home);
-
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        //Init component
-        listViewSliding = (ListView) findViewById(R.id.lv_sliding_menu);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        listSliding = new ArrayList<>();
-        //Add item for sliding list
-        listSliding.add(new ItemSlideMenu(R.drawable.ic_action_settings, "Home"));
-        listSliding.add(new ItemSlideMenu(R.drawable.ic_action_settings, "Profile"));
-        listSliding.add(new ItemSlideMenu(R.drawable.ic_action_settings, "Settings"));
-        listSliding.add(new ItemSlideMenu(R.drawable.ic_action_about, "About"));
-        listSliding.add(new ItemSlideMenu(R.drawable.ic_action_settings, "Log Out"));
-        adapter = new SlidingMenuAdapter(this, listSliding);
-        listViewSliding.setAdapter(adapter);
-
-        //Display icon to open/ close sliding list
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar = (Toolbar) findViewById(R.id.tToolbar);
+        setSupportActionBar(toolbar);
 
 
-        //Set title
-        setTitle(listSliding.get(0).getTitle());
-        //item selected
-        listViewSliding.setItemChecked(0, true);
-        //Close menu
-        drawerLayout.closeDrawer(listViewSliding);
 
-        //Display fragment 1 when start
-        //replaceFragment(0);
-        //Hanlde on item click
 
-        listViewSliding.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Set title
-                setTitle(listSliding.get(position).getTitle());
-                //item selected
-                listViewSliding.setItemChecked(position, true);
-                //Replace fragment
-                replaceFragment(position);
-                //Close menu
-                drawerLayout.closeDrawer(listViewSliding);
-            }
-        });
 
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_opened, R.string.drawer_closed){
 
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                invalidateOptionsMenu();
-            }
 
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-                invalidateOptionsMenu();
-            }
-        };
-
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
     }
 
     @Override
-    public boolean onOptionsItemSelected(android.view.MenuItem item) {
-        return actionBarDrawerToggle.onOptionsItemSelected(item);
-    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_home,menu);
 
+        return true;
+    }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        actionBarDrawerToggle.syncState();
-    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int res_id = item.getItemId();
+        if(res_id == R.id.action_profile)
+        {
+            Intent profileActivity = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(profileActivity);
+            finish();
+            overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
 
-    private void replaceFragment(int pos) {
-        Fragment fragment = null;
-        switch (pos) {
-            case 0:
-                fragment = new Fragment1();
-                break;
-            case 1:
-                fragment = new Fragment2();
-                break;
-            case 2:
-                fragment = new Fragment3();
-                break;
-            default:
-                fragment = new Fragment1();
-                break;
+
+        }
+        else if(res_id == R.id.action_home)
+        {
+
+        }
+        else if(res_id == R.id.action_msg)
+        {
+            Intent cnectActivity = new Intent(HomeActivity.this, ConnectionsActivity.class);
+            startActivity(cnectActivity);
+            finish();
+            overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
         }
 
-        if(null!=fragment) {
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.main_content, fragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
+        return true;
     }
 }
