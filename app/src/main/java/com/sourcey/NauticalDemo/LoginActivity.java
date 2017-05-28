8d00a27f397288d8e37660e5f08964940d05d5da
Dickturd <senzafessa@gmail.com>
2017-05-28 17:56:14 -0500

    s

--- app/src/main/java/com/sourcey/NauticalDemo/LoginActivity.java
+++ app/src/main/java/com/sourcey/NauticalDemo/LoginActivity.java
@@ -32,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     @Bind(R.id.input_password) EditText _passwordText;
     @Bind(R.id.btn_login) Button _loginButton;
     @Bind(R.id.link_signup) TextView _signupLink;
+    @Bind(R.id.link_forgot) TextView _forgotLink;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -76,6 +77,18 @@ public class LoginActivity extends AppCompatActivity {
                 // Start the Signup activity
                 Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivityForResult(intent, REQUEST_SIGNUP);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+
+        _forgotLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the ForgotPass activity
+                Intent forgot = new Intent(getApplicationContext(),ForgotPasswordActivity.class);
+                startActivityForResult(forgot, REQUEST_SIGNUP);
                 finish();
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
